09748e6c19ef1fb1ccded1e550f8fc291b8d571d
Itspyder <haydenjoffe@gmail.com>
2023-06-09 13:54:02 -0400

    new dictionary for pathways

--- ComputerVision/BackEnd/App.py
+++ ComputerVision/BackEnd/App.py
@@ -2,6 +2,37 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+asl_mapping = {
+    'A': 'ComputerVision/BackEnd/static/asl.alpha.images/A.jpg',
+    'B': 'ComputerVision/BackEnd/static/asl.alpha.images/B.jpg',
+    'C': 'ComputerVision/BackEnd/static/asl.alpha.images/C.jpg',
+    'D': 'ComputerVision/BackEnd/static/asl.alpha.images/D.jpg',
+    'E': 'ComputerVision/BackEnd/static/asl.alpha.images/E.jpg',
+    'F': 'ComputerVision/BackEnd/static/asl.alpha.images/F.jpg',
+    'G': 'ComputerVision/BackEnd/static/asl.alpha.images/G.jpg',
+    'H': 'ComputerVision/BackEnd/static/asl.alpha.images/H.jpg',
+    'I': 'ComputerVision/BackEnd/static/asl.alpha.images/I.jpg',
+    'J': 'ComputerVision/BackEnd/static/asl.alpha.images/J.jpg',
+    'K': 'ComputerVision/BackEnd/static/asl.alpha.images/K.jpg',
+    'L': 'ComputerVision/BackEnd/static/asl.alpha.images/L.jpg',
+    'M': 'ComputerVision/BackEnd/static/asl.alpha.images/M.jpg',
+    'N': 'ComputerVision/BackEnd/static/asl.alpha.images/N.jpg',
+    'O': 'ComputerVision/BackEnd/static/asl.alpha.images/O.jpg',
+    'P': 'ComputerVision/BackEnd/static/asl.alpha.images/P.jpg',
+    'Q': 'ComputerVision/BackEnd/static/asl.alpha.images/Q.jpg',
+    'R': 'ComputerVision/BackEnd/static/asl.alpha.images/R.jpg',
+    'S': 'ComputerVision/BackEnd/static/asl.alpha.images/S.jpg',
+    'T': 'ComputerVision/BackEnd/static/asl.alpha.images/T.jpg',
+    'U': 'ComputerVision/BackEnd/static/asl.alpha.images/U.jpg',
+    'V': 'ComputerVision/BackEnd/static/asl.alpha.images/V.jpg',
+    'W': 'ComputerVision/BackEnd/static/asl.alpha.images/W.jpg',
+    'X': 'ComputerVision/BackEnd/static/asl.alpha.images/X.jpg',
+    'Y': 'ComputerVision/BackEnd/static/asl.alpha.images/Y.jpg',
+    'Z': 'ComputerVision/BackEnd/static/asl.alpha.images/Z.jpg',
+}
+
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -20,10 +51,11 @@ def asl_to_text():
     return render_template('asl_to_text.html')
 
 def textToList(message):
-    # Perform processing logic here
-    # Example: Separate the message into lists
-    asl_lists = message.split(' ')
-    return asl_lists
+    translated_images = []
+    for word in message.split():
+        asl_images = [asl_mapping.get(letter.upper()) for letter in word]
+        translated_images.extend(asl_images)
+    return translated_images
 
 if __name__ == '__main__':
     app.run()
